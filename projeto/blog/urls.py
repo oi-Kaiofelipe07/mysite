8330f1648393b551e834515abda3ba818682b11f
Kaio Felipe <kaiofelipee12@gmail.com>
2025-10-07 11:22:43 -0300

    django-admin

--- projeto/blog/urls.py
+++ projeto/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from projeto.blog.views.post_views import PostView
+from blog.views.post_views import PostView
 
 urlpatterns = [
     path('', PostView.as_view(), name='home')
